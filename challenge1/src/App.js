50feb06bfea82480e166c7addd57987cf76392e3
Ambarish Bhagawati <ambarish8653@gmail.com>
2023-07-29 14:53:02 +0530

    challenge 1 folder added

---
+++ challenge1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { add,sub,mul,div } from './Calculator';
+
+const cssStyle={
+  textAlign:'center'
+}
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;

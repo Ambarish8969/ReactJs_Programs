08882a9a309ac65400e5b29e320d45d6fb856f08
Ambarish Bhagawati <ambarish8653@gmail.com>
2023-07-30 11:07:37 +0530

    files updated

--- challenge1/src/index.js
+++ challenge1/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 // import Challenge1 from './Challenge1'
-import Home from './Axios1'
+// import Home from './Axios1'
 
 ReactDOM.render(
-    <>
-        <Home/>
-        {/* <Challenge1/> */}
-    </>
-    ,
-    document.getElementById("root")
-)
+  <>
+    {/* <Home/> */}
+    {/* <Challenge1/> */}
+    <Navbar />
+    <Footer />
+  </>,
+  document.getElementById("root")
+);
